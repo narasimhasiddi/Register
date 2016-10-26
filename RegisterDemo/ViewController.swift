3090aede87e5e0ec1cba7c47350c76a7fea33a97
narasimhaqwinix <snreddy@qwinix.io>
2016-10-26 14:50:28 +0530

    Added initial comments

--- RegisterDemo/ViewController.swift
+++ RegisterDemo/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Added initial comment
     }
 
     override func didReceiveMemoryWarning() {
